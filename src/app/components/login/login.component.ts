37bda18df7b6c979cee5dc374707cf7440ad8a50
helelys <tallyslabanca20@gmail.com>
2025-06-03 12:04:01 -0300

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,31 +2,39 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ApiServiceService } from '../../service/api-service.service';
-import { User } from '../../service/User';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   email: string = '';
   password: string = '';
 
-  constructor(private apiService: ApiServiceService) { }
+  constructor(private apiService: ApiServiceService) {}
 
   login() {
     const credentials = {
       email: this.email,
-      password: this.password
+      password: this.password,
     };
 
     this.apiService.loginUser(credentials).subscribe({
-      next: (response) => console.log('Login successful', response),
-      error: (error) => console.error('Login failed', error)
+      next: (response: any) => {
+        console.log('Login successful', response);
+        sessionStorage.setItem('user', JSON.stringify(response));
+        const userData = {
+          id: response.user.id,
+          email: response.user.email,
+          name: response.user.name,
+          role: response.user.role,
+        };
+      },
+      error: (error) => console.error('Login failed', error),
     });
   }
 }
